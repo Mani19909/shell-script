ce68e5d367076ab18354fd5ca08a0a843c097f74
Mani19909 <127381119+Mani19909@users.noreply.github.com>
2026-02-05 13:07:55 +0530

    spelling mistakes in services.sh script

--- arrays/services.sh
+++ arrays/services.sh
@@ -1,9 +1,9 @@
-# check multiple services
 #!/bin/bash
+# check multiple services
 services=("nginx" "docker" "ssh")
 for s in "${services[@]}"
 do 
-if f systemctl is-active --quiet "$s"; then
+if systemctl is-active --quiet "$s"; then
 echo "$s is running"
 else 
 echo "$s is not running"
