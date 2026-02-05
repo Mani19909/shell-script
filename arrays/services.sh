35cbc53489e72cc4a699cfec0afc87a56f9da852
Mani19909 <127381119+Mani19909@users.noreply.github.com>
2026-02-05 13:05:23 +0530

    Fix syntax error in services.sh

--- arrays/services.sh
+++ arrays/services.sh
@@ -3,6 +3,10 @@
 services=("nginx" "docker" "ssh")
 for s in "${services[@]}"
 do 
-systemctl is-active --quiet $s
+if f systemctl is-active --quiet "$s"; then
+echo "$s is running"
+else 
+echo "$s is not running"
+fi
 done
 
