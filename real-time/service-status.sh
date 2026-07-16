376576ea2b8c8f8ccd9355d4b691a3882e03f259
manikanta Routhu <manikantar129@gmail.com>
2026-07-16 12:25:44 +0530

    shell script

--- real-time/service-status.sh
+++ real-time/service-status.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "Please run this script with root access."
+exit 1
+else
+echo "you are in super user."
+fi
+
 SERVICE="nginx"
 if ! pgrep -x "$SERVICE" > /dev/null; then
     echo "$SERVICE not running, starting it..."
