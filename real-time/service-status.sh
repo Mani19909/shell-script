ca378cb2385027d04f36d647807291edc6cb1763
manikanta Routhu <manikantar129@gmail.com>
2026-07-16 11:54:47 +0530

    shell script

---
+++ real-time/service-status.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+SERVICE="nginx"
+if ! pgrep -x "$SERVICE" > /dev/null; then
+    echo "$SERVICE not running, starting it..."
+    systemctl start $SERVICE
+else
+    echo "$SERVICE is running"
+fi
